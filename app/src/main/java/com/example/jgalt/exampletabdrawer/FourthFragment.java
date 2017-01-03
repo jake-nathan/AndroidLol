df4d5cb4a7beafaf0fd0276a1bf4ee04ef49eb7e
jake-nathan <jgalt@Irls-MacBook-Pro-3.local>
2017-01-02 22:45:14 -0600

    Tab Drawer basic

---
+++ app/src/main/java/com/example/jgalt/exampletabdrawer/FourthFragment.java
@@ -0,0 +1,22 @@
+package com.example.jgalt.exampletabdrawer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jgalt on 1/2/17.
+ */
+
+public class FourthFragment extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.fourth_layout, container, false);
+        return myView;
+}
+}
